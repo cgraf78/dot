e5028143bafce13a75fa4044730042c6ed5cf9b2
Chris Graf <chris@grafhome.net>
2026-09-14 08:21:01 -0500

    Revert "Pin parallel overlay pull parity for dot update (#152)"
    This reverts commit c00d085604bea18607a8365c233989c031767ab6.

--- src/lib.rs
+++ src/lib.rs
@@ -30,7 +30,6 @@ pub mod init_client_adopt;
 pub mod init_client_candidate;
 pub mod init_client_command;
 pub mod init_client_delete;
-pub mod init_client_engine;
 pub mod init_client_entry;
 pub mod init_client_generation;
 pub mod init_client_git;
@@ -83,7 +82,6 @@ pub mod test_support;
 pub mod ui;
 pub mod update;
 pub mod update_lock;
-pub mod update_run;
 pub mod version;
 pub mod xdg;
 
